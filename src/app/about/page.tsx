7ae0d2ccb9d330c67660f355d5e3dd1ea4032d5b
Nicolae Sircu <sircuniku1@gmail.com>
2025-02-24 11:25:54 +0200

    Fetch data

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-  return <div>Hello</div>;
+export default async function Page() {
+  const url = `https://catfact.ninja/fact`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return <div style={{ color: "white" }}>{data.fact}</div>;
 }
